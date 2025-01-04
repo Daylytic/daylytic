e9d7b517a872a94a5eed05694b4cb14861dfec36
iDarkQ <matikuki3@gmail.com>
2025-01-04 21:58:18 +0000

    feat(layout): utilize all new providers in the panel and export all of the new layouts in the index.ts file

--- apps/web/src/components/layout/layout-panel.tsx
+++ apps/web/src/components/layout/layout-panel.tsx
@@ -1,36 +1,74 @@
 import { Layout as AntLayout } from "antd";
 import { styles } from ".";
 import { useCookies } from "react-cookie";
-import { DailyTasksProvider } from "providers/daily-tasks";
-import { TagProvider } from "providers/tag";
+import { TagProvider } from "~/providers/tag";
 import { Outlet } from "react-router";
-import { Menu } from "components/panel/menu";
-import { GoalProvider } from "providers/goal";
-import { PanelFetcherProvider } from "providers/panel-fetcher";
-import { ProjectProvider } from "providers/project";
-import { TaskProvider } from "providers/task";
-import { SelectedTaskProvider } from "providers/selected-task";
+import { Menu } from "~/components/panel/menu";
+import { GoalProvider } from "~/providers/goal";
+import { PanelFetcherProvider } from "~/providers/panel-fetcher";
+import { ProjectProvider } from "~/providers/project";
+import { TaskProvider } from "~/providers/task";
+import { SelectedTaskProvider } from "~/providers/selected-task";
+import { AnalyticsProvider } from "~/providers/analytics";
+import { TimelyticProvider } from "~/providers/timelytic";
+import { AssistanceProvider } from "~/providers/assistance";
+import { ReactNode } from "react";
+import { LayoutProvider } from "~/providers/layout";
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import { TourProvider } from "~/providers/tour";
+import { GlobalTour } from "~/providers/tour/global-tour";
 
-export const LayoutPanel = () => {
+interface PanelDataWrapperProps {
+  children: ReactNode;
+}
+
+export const PanelDataWrapper = ({ children }: PanelDataWrapperProps) => {
   const [cookies] = useCookies(["token"]);
+  NProgress.configure({
+    minimum: 0.1,
+    easing: "ease",
+    speed: 500,
+    trickle: true,
+    trickleSpeed: 200,
+    showSpinner: false,
+  });
+
   return (
     <PanelFetcherProvider token={cookies.token}>
-      <GoalProvider token={cookies.token}>
-        <ProjectProvider>
-          <TaskProvider>
-            <SelectedTaskProvider>
-              <DailyTasksProvider token={cookies.token}>
-                <TagProvider token={cookies.token}>
-                  <AntLayout id={styles.panel}>
-                    <Menu />
-                    <Outlet />
-                  </AntLayout>
-                </TagProvider>
-              </DailyTasksProvider>
-            </SelectedTaskProvider>
-          </TaskProvider>
-        </ProjectProvider>
-      </GoalProvider>
+      <AnalyticsProvider>
+        <AssistanceProvider>
+          <TimelyticProvider>
+            <GoalProvider>
+              <ProjectProvider>
+                <TaskProvider>
+                  <SelectedTaskProvider>
+                    <TagProvider>
+                      <LayoutProvider>
+                        <TourProvider>
+                          <AntLayout id={styles.panel}>{children}</AntLayout>
+                          <GlobalTour />
+                        </TourProvider>
+                      </LayoutProvider>
+                    </TagProvider>
+                  </SelectedTaskProvider>
+                </TaskProvider>
+              </ProjectProvider>
+            </GoalProvider>
+          </TimelyticProvider>
+        </AssistanceProvider>
+      </AnalyticsProvider>
     </PanelFetcherProvider>
+  );
+};
+
+export const LayoutPanel = () => {
+  // const {lg} =
+
+  return (
+    <PanelDataWrapper>
+      <Menu />
+      <Outlet />
+    </PanelDataWrapper>
   );
 };
