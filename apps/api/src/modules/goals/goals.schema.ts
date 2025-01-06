d0adcfa800b506141582d3a2992d9039fbaa8ad0
iDarkQ <matikuki3@gmail.com>
2025-01-06 07:55:17 +0000

    refactor(goals.schema.ts): finish basic schemas for body and response

--- apps/api/src/modules/goals/goals.schema.ts
+++ apps/api/src/modules/goals/goals.schema.ts
@@ -1,39 +1,22 @@
 import { buildJsonSchemas } from "fastify-zod";
 import {z} from "zod";
 
-const createGoalSchema = z.object({
+const goalInput = z.object({
     title: z.string(),
-    description: z.string(),  
+    description: z.string(),
+    deadline: z.string().optional(),
+    progress: z.number().optional(),
 })
 
-const createGoalSchemaResponse = z.object({
-
+const goalResponse = goalInput.extend({
     id: z.string(),
-    createGoalSchema,
-    deadline: z.string(),
-    progress: z.number(),
     createdAt: z.string(),
     updatedAt: z.string(),
-    userId: z.string(),
-
 })
 
-// model Goal {
-//     id          String    @id @default(cuid()) // Unique ID for the goal
-//     title       String // Title of the goal
-//     description String? // Optional description
-//     deadline    DateTime? // Optional deadline for the goal
-//     progress    Float     @default(0) // Progress percentage (0 to 100)
-//     createdAt   DateTime  @default(now()) // Creation timestamp
-//     updatedAt   DateTime  @updatedAt // Auto-updated when modified
-//     userId      String // Relation field to User
-//     user        User      @relation(fields: [userId], references: [id])
-//     projects    Project[] // One goal can have multiple projects
-//   }
-
-export type CreateGoalInput = z.infer<typeof createGoalSchemaResponse>;
+export type CreateGoalInput = z.infer<typeof goalResponse>;
 
 export const {schemas: goalSchemas, $ref} = buildJsonSchemas({
-    createGoalSchema,
-    createGoalSchemaResponse
-})
+    goalInput,
+    goalResponse
+}, {$id: "GoalsSchema"})
