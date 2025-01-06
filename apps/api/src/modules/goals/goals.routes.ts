eaef9280c0ecef622cd56e6e1345a1f169c7e3bf
iDarkQ <matikuki3@gmail.com>
2025-01-06 07:55:41 +0000

    refactor(goals.route.ts): include schemas in the goalsRouter

--- apps/api/src/modules/goals/goals.routes.ts
+++ apps/api/src/modules/goals/goals.routes.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsync, FastifyRequest } from "fastify";
 import { createGoal } from "./goals.controller.js";
+import { $ref } from "./goals.schema.js";
 
-export const goalsHandler: FastifyPluginAsync = async (server, options) => {
-    server.route({ url: "/", method: "POST", handler: createGoal })
+export const goalsHandler: FastifyPluginAsync = async (server) => {
+    server.route({ url: "/", method: "POST", handler: createGoal, schema: {
+        body: $ref("goalInput"),
+        response: {
+            201: $ref("goalResponse"),
+        }
+    } })
 }
