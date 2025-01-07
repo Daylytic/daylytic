0e8c67147cdd69a24d1d496dd334f840364beaa0
iDarkQ <matikuki3@gmail.com>
2025-01-07 09:02:59 +0000

    feat(server.ts): register swagger and swagger ui

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,4 +1,6 @@
 import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 import postgres from '@fastify/postgres'; // Use import instead of requird
 import { authHandler } from './src/modules/auth/index.js'; // Ensure the extension is included
 import { SessionCore, UserCore, userSchemas } from './src/modules/auth/auth.schema.js';
@@ -16,7 +18,6 @@ const server = Fastify({ logger: true })
 
 const main = async () => {
 
-    
   for (const schema of [...userSchemas, ...goalSchemas]) {
     server.addSchema(schema);
   }
@@ -31,6 +32,52 @@ const main = async () => {
     }
   });
 
+  await server.register(swagger, {
+    openapi: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Daylytic Swagger',
+        description: 'API for Daylytic',
+        version: '0.1.0'
+      },
+      servers: [
+        {
+          url: 'http://localhost:8084',
+          description: 'Development server'
+        }
+      ],
+      tags: [
+        { name: 'auth', description: 'Authentication related end-points' },
+        { name: 'goals', description: 'Goals related end-points' }
+      ],
+      components: {
+        securitySchemes: {
+          apiKey: {
+            type: 'apiKey',
+            name: 'apiKey',
+            in: 'header'
+          }
+        }
+      }
+    }
+  });
+
+  await server.register(swaggerUi, {
+    routePrefix: '/documentation',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) { next() },
+      preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
+  })
+
   server.register(authHandler, { prefix: "/oauth2" });
   server.register(goalsHandler, { prefix: "/goals" });
 
@@ -41,6 +88,8 @@ const main = async () => {
     console.error(err);
     process.exit(1)
   }
+  await server.ready()
+  server.swagger()
 }
 
 main();
