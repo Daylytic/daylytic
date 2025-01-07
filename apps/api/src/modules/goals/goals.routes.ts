8fa288cd92c8517d6291c9f7ebcc4862f27e4dde
iDarkQ <matikuki3@gmail.com>
2025-01-07 09:02:33 +0000

    refactor(modules): improve readability of the routes and schemas

--- apps/api/src/modules/goals/goals.routes.ts
+++ apps/api/src/modules/goals/goals.routes.ts
@@ -6,6 +6,7 @@ import { authController } from "../auth/auth.controller.js";
 export const goalsHandler: FastifyPluginAsync = async (server) => {
     server.route({
         url: "/", method: "POST", preHandler: authController.authenticate, handler: createGoal, schema: {
+            tags: ["goals"],
             body: $ref("goalInput"),
             response: {
                 201: $ref("goalResponse"),
