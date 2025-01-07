34a0c73e1978b996b95fc060215decb6ef2358a2
iDarkQ <matikuki3@gmail.com>
2025-01-07 16:36:23 +0000

    feat(goals): finish creating, deleting and reading goals

--- apps/api/src/modules/goals/goals.service.ts
+++ apps/api/src/modules/goals/goals.service.ts
@@ -1,27 +1,36 @@
 import { prisma } from "../../utils/prisma.js"
 import { UserCore } from "../auth/auth.schema.js"
-import { CreateGoalInput } from "./goals.schema.js";
+import { CreateGoalInput, GoalCore } from "./goals.schema.js";
 
-const getGoals = (user: UserCore) => {
+const getGoals = async(user: UserCore): Promise<GoalCore[]> => {
+    const goals = await prisma.goal.findMany({where: {
+        userId: user.id
+    }})
+
+    return goals!;
+}
+
+const createGoal = async (input: CreateGoalInput) => {
     try {
-        const goals = prisma.goal.findMany({where: {
-            userId: user.id
-        }})
-
-        return goals;
+        return await prisma.goal.create({data: input});
     }catch(err) {
-        console.error(err);
+        throw err;
     }
 }
 
-const createGoal = (input: CreateGoalInput) => {
+const deleteGoal = async (userId: string, goalId: string) => {
     try {
-        // prisma.goal.create({data: input});
-    }catch(err) {
-        console.log(err);
+        await prisma.goal.delete({where: {
+            id: goalId,
+            userId: userId
+        }});
+    } catch(err) {
+        throw Error("Could not find goal with given id.")
     }
 }
 
 export const goalsService = {
-    getGoals
+    getGoals,
+    createGoal,
+    deleteGoal,
 }
