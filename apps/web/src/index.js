79729a1d3b2d92de408b55ec3637d8c9b79b0f8e
iDarkQ <matikuki3@gmail.com>
2025-01-08 09:10:46 +0000

    feat(user-provider.tsx): create user provider

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -4,14 +4,17 @@ import "./assets/styles/global.css";
 import { App } from "./app.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { CookiesProvider } from "react-cookie";
+import { UserProvider } from "./providers/user-provider.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="706835003528-uk37jmqoc4str6spfv0l37mdoi4jqais.apps.googleusercontent.com">
-    <CookiesProvider defaultSetOptions={{path: "/"}}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+    <CookiesProvider defaultSetOptions={{ path: "/" }}>
+      <UserProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </UserProvider>
     </CookiesProvider>
   </GoogleOAuthProvider>
 );
