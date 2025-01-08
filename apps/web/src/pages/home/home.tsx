2f43ce571a171c15e1c245c7cac6169fc89f1908
iDarkQ <matikuki3@gmail.com>
2025-01-08 09:15:10 +0000

    refactor(home.tsx): add About, FAQ, and Contact sections to the home page

--- apps/web/src/pages/home/home.tsx
+++ apps/web/src/pages/home/home.tsx
@@ -1,11 +1,17 @@
 import { DatePicker, Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
-import { Hero } from "components/home/hero/hero";
+import { About } from "components/home/about";
+import { Contact } from "components/home/contact/contact";
+import { FAQ } from "components/home/faq";
+import { Hero } from "components/home/hero";
 
 export const Home = () => (
   <Layout>
     <Content>
       <Hero />
+      <About />
+      <FAQ />
+      <Contact />
     </Content>
   </Layout>
 );
