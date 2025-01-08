b7f951a0bd2d5bd7656d855378e9f4da421d6572
iDarkQ <matikuki3@gmail.com>
2025-01-08 09:07:45 +0000

    feat(projects): add post request & improve retrieving existing projects

--- apps/api/src/modules/goals/goals.service.ts
+++ apps/api/src/modules/goals/goals.service.ts
@@ -2,29 +2,43 @@ import { prisma } from "../../utils/prisma.js"
 import { UserCore } from "../auth/auth.schema.js"
 import { CreateGoalInput, GoalCore } from "./goals.schema.js";
 
-const getGoals = async(user: UserCore): Promise<GoalCore[]> => {
-    const goals = await prisma.goal.findMany({where: {
-        userId: user.id
-    }})
+const getGoal = async (goalId: string): Promise<GoalCore> => {
+    try {
+        return await prisma.goal.findFirstOrThrow({where: {
+            id: goalId
+        }});
+    }catch(err) {
+        throw Error("Could not find goal with given id");
+    }
+}
+
+const getGoals = async (user: UserCore): Promise<GoalCore[]> => {
+    const goals = await prisma.goal.findMany({
+        where: {
+            userId: user.id
+        }
+    })
 
     return goals!;
 }
 
 const createGoal = async (input: CreateGoalInput) => {
     try {
-        return await prisma.goal.create({data: input});
-    }catch(err) {
+        return await prisma.goal.create({ data: input });
+    } catch (err) {
         throw err;
     }
 }
 
 const deleteGoal = async (userId: string, goalId: string) => {
     try {
-        await prisma.goal.delete({where: {
-            id: goalId,
-            userId: userId
-        }});
-    } catch(err) {
+        await prisma.goal.delete({
+            where: {
+                id: goalId,
+                userId: userId
+            }
+        });
+    } catch (err) {
         throw Error("Could not find goal with given id.")
     }
 }
@@ -33,4 +47,5 @@ export const goalsService = {
     getGoals,
     createGoal,
     deleteGoal,
+    getGoal,
 }
--- apps/api/src/modules/projects/projects.controller.ts
+++ apps/api/src/modules/projects/projects.controller.ts
@@ -1,18 +1,25 @@
 import { FastifyReply, FastifyRequest } from "fastify"
-import { ProjectsQuery } from "./projects.schema.js"
+import { ProjectsBaseQuery } from "./projects.schema.js"
+import { projectsService } from "./projects.service.js"
+import { goalsService } from "../goals/goals.service.js"
 
-const getProjects = async() => {
+const getProjects = async (req: FastifyRequest, rep: FastifyReply) => {
+    const { goalId } = req.query as ProjectsBaseQuery;
+
+    return projectsService.getProjects(goalId);
+}
+
+const createProject = async () => {
 
 }
 
-const createProject = async() => {
+const verifyGoalId = async (req: FastifyRequest, rep: FastifyReply) => {
+    const { goalId } = req.query as ProjectsBaseQuery;
+    const goal = await goalsService.getGoal(goalId);
 
-}
-
-const verifyGoalId = async(req: FastifyRequest, rep: FastifyReply) => {
-    const { goalId } = req.query as ProjectsQuery;
-
-    
+    if (goal.userId !== req.user!.id) {
+        return rep.status(401).send({ error: "Unauthorized" });
+    }
 }
 
 export const projectsController = {
