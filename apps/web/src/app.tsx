4cf38935d8efc4ebe49b6d03776a2fab816d0051
iDarkQ <matikuki3@gmail.com>
2025-01-09 09:21:22 +0000

    refactor(apps.tsx): update config settings

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -13,15 +13,20 @@ export const App = () => (
       token: {
         // Seed Token
         colorPrimary: "#6247aa",
-        borderRadius: 2,
-        colorWhite: "#f5f5f5",
+        borderRadius: 5,
         fontFamily: "Poppins",
+        colorBgContainer: "white",
+        colorBgLayout: "#f5f5f5",
+        colorWhite: "#fafafa",
       },
       components: {
         Layout: {
           headerBg: "#f5f5f5",
           headerColor: "#f5f5f5",
           headerPadding: 0,
+          // colorBgContainer: "#fafafa",
+          bodyBg: "#fafafa",
+          siderBg: "#f5f5f5",
         },
         Menu: {
           popupBg: "#f5f5f5",
