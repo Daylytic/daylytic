25d5cc38b52ed697600a0f5c0229274932237288
iDarkQ <matikuki3@gmail.com>
2025-01-09 16:34:37 +0000

    refactor(panel.tsx): replace goals with panel

---
+++ apps/web/src/pages/panel/panel.tsx
@@ -0,0 +1,27 @@
+import { Flex, FloatButton, Layout, theme } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import { ContentRoutine } from "components/panel/content/content-routine";
+import { Menu } from "components/panel/menu";
+
+export const Panel = () => {
+  const {
+    token: { borderRadiusLG, paddingMD },
+  } = theme.useToken();
+
+  return (
+    <Layout
+      style={{
+        padding: "24px 24px",
+        borderRadius: borderRadiusLG,
+        height: "100vh",
+      }}
+    >
+      <Flex gap={paddingMD} flex={1} style={{height: "100%"}}>
+        <Menu />
+        <ContentRoutine></ContentRoutine>
+        <Sider style={{}} width={500}></Sider>
+      </Flex>
+    </Layout>
+  );
+};
