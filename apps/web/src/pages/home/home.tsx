e9b659e65abb665ce36258e3e031fe05e59c5181
iDarkQ <matikuki3@gmail.com>
2025-01-10 16:13:05 +0000

    style(home.tsx): remove useless imports

--- apps/web/src/pages/home/home.tsx
+++ apps/web/src/pages/home/home.tsx
@@ -1,7 +1,5 @@
-import { DatePicker } from "antd";
-import { Content } from "antd/es/layout/layout";
 import { About } from "components/home/about";
-import { Contact } from "components/home/contact/contact";
+import { Contact } from "components/home/contact";
 import { FAQ } from "components/home/faq";
 import { Hero } from "components/home/hero";
 import { Layout } from "components/layout";
