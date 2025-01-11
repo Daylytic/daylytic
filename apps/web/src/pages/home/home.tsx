b839ff6d7f2cc960fa391f3bbe5bdc460f95cea6
iDarkQ <matikuki3@gmail.com>
2025-01-11 23:19:04 +0000

    refactor(home.tsx): change layout name

--- apps/web/src/pages/home/home.tsx
+++ apps/web/src/pages/home/home.tsx
@@ -2,13 +2,13 @@ import { About } from "components/home/about";
 import { Contact } from "components/home/contact";
 import { FAQ } from "components/home/faq";
 import { Hero } from "components/home/hero";
-import { Layout } from "components/layout";
+import { LayoutHome } from "components/layout";
 
 export const Home = () => (
-  <Layout>
+  <LayoutHome>
     <Hero />
     <About />
     <FAQ />
     <Contact />
-  </Layout>
+  </LayoutHome>
 );
