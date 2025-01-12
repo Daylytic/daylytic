854927dda4f090eaa70f9f9310240baf2281a1c1
iDarkQ <matikuki3@gmail.com>
2025-01-12 23:54:34 +0000

    feat(action): start preparing for the right sider

---
+++ apps/web/src/components/panel/action/action.tsx
@@ -0,0 +1,14 @@
+import Sider from "antd/es/layout/Sider";
+import styles from "./action.module.css";
+import { useMenuController } from "providers/menu-controller";
+
+export const Action = () => {
+  //   const { action, setAction } = useMenuController();
+
+  //   if (action === undefined) {
+  //     return;
+  //   }
+
+  //   return <Sider width={500} id={styles.sider}></Sider>;
+  return <></>;
+};
