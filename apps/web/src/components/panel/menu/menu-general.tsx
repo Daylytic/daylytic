9b6edfcc1b5f9c6de978b0c82b55a545444a157b
iDarkQ <matikuki3@gmail.com>
2025-01-12 23:55:40 +0000

    refactor(menu-items.tsx): move items from menu to spare file

--- apps/web/src/components/panel/menu/menu-general.tsx
+++ apps/web/src/components/panel/menu/menu-general.tsx
@@ -1,86 +1,15 @@
-import {
-  ScheduleOutlined,
-  ClockCircleOutlined,
-  UnorderedListOutlined,
-  ForwardOutlined,
-  CalendarOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import { Flex, Badge, Menu, Typography, theme, Button } from "antd";
-import { MenuGeneralItem } from "./menu-general-item";
-import logo from "assets/svgs/logo.svg";
+import { Menu } from "antd";
+import { useNavigate } from "react-router";
+import { getGeneralMenuItems } from "utils/menu-items";
 import { useMenuController } from "providers/menu-controller";
 
-const { Title } = Typography;
-
-const general = [
-  {
-    key: `dashboard`,
-    label: <MenuGeneralItem text="Dashboard" />,
-    icon: <ScheduleOutlined />,
-  },
-  {
-    //Routine PLUS if finished routine, green timer means a lot of time left, yellow means less time left, red means little to no time left
-    key: `routine`,
-    label: (
-      <MenuGeneralItem
-        text="Routine"
-        badge={
-          <Badge count={<ClockCircleOutlined style={{ color: "#f5222d" }} />} />
-        }
-      />
-    ),
-    icon: <UnorderedListOutlined />,
-  },
-  {
-    key: `upcoming`,
-    label: <MenuGeneralItem text="Upcoming" />,
-    icon: <ForwardOutlined />,
-  },
-  {
-    key: `calendar`,
-    label: <MenuGeneralItem text="Calendar" />,
-    icon: <CalendarOutlined />,
-  },
-];
-
 export const MenuGeneral = () => {
-  const { setMenu, menu } = useMenuController();
+  const navigate = useNavigate();
+  const {menu, setMenu} = useMenuController();
 
-  const {
-    token: { fontSizeHeading4, paddingXS },
-  } = theme.useToken();
+  const generalItems = getGeneralMenuItems();
 
   return (
-    <>
-      <Flex
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 15,
-        }}
-      >
-        <Flex gap={paddingXS}>
-          <img height="38px" src={logo} />
-          <Title level={2} style={{ marginBottom: 0 }}>
-            Daylytic
-          </Title>
-        </Flex>
-        <Button shape="circle" type="text">
-          <SettingOutlined style={{ fontSize: fontSizeHeading4 }} />
-        </Button>
-      </Flex>
-      <Menu
-        mode="inline"
-        defaultSelectedKeys={[menu]}
-        selectedKeys={[menu]}
-        defaultOpenKeys={["dashboard"]}
-        onClick={({ item, key, keyPath }) => {
-          setMenu(key);
-        }}
-        items={general}
-      />
-    </>
+    <Menu
   );
 };
