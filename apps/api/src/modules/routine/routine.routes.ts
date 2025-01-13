ff7900d2fdb47ede9f226f50ca651ffc2cf0556d
iDarkQ <matikuki3@gmail.com>
2025-01-13 17:58:14 +0000

    feat(routine.routes.ts): create router for routines

---
+++ apps/api/src/modules/routine/routine.routes.ts
@@ -0,0 +1,51 @@
+import { FastifyPluginAsync } from "fastify";
+import { authController } from "../auth/auth.controller.js";
+import { $ref, routineController } from "./index.js";
+import { $ref as $refAuth } from '../auth/auth.schema.js';
+
+export const routineHandler: FastifyPluginAsync = async (server, _) => {
+  /* Create task */
+  server.route({
+    url: "/",
+    method: "POST",
+    preHandler: authController.authenticate,
+    handler: routineController.createDailyTask,
+    schema: {
+      tags: ["routine"],
+
+      body: $ref("createDailyTaskInput"),
+      headers: $refAuth("headersBearer"),
+      response: {
+        201: $ref("createDailyTaskResponse"),
+      },
+    },
+  });
+
+  /* Fetch tasks */
+  server.route({
+    url: "/",
+    method: "GET",
+    preHandler: authController.authenticate,
+    handler: routineController.getDailyTasks,
+    schema: {
+      tags: ["routine"],
+      headers: $refAuth("headersBearer"),
+      response: {
+        200: $ref("fetchDailyTasksResponse"),
+      },
+    },
+  });
+
+  /* Delete Task */
+  server.route({
+    url: "/",
+    method: "DELETE",
+    preHandler: authController.authenticate,
+    handler: routineController.deleteDailyTask,
+    schema: {
+      tags: ["routine"],
+      headers: $refAuth("headersBearer"),
+      body: $ref("deleteDailyTaskInput"),
+    },
+  });
+};
