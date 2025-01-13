d3ee11bc8e1eff1527c3c790c9b6b41a70f111ab
iDarkQ <matikuki3@gmail.com>
2025-01-13 17:59:06 +0000

    feat(routine.schema.ts): create schema for routine endpoint

---
+++ apps/api/src/modules/routine/routine.schema.ts
@@ -0,0 +1,46 @@
+import { buildJsonSchemas } from "fastify-zod";
+import { z } from "zod";
+
+/* CORE */
+
+const dailyTaskCore = z.object({
+    id: z.string(),
+    title: z.string(),
+    isCompleted: z.boolean(),
+    userId: z.string(),
+});
+
+/* CREATE DAILY TASK */
+
+const createDailyTaskInput = z.object({
+    title: z.string(),
+});
+
+const createDailyTaskDetailed = createDailyTaskInput.extend({
+    userId: z.string(),
+})
+
+const createDailyTaskResponse = dailyTaskCore;
+
+/* FETCH DAILY TASKS */
+
+const fetchDailyTasksResponse = z.array(dailyTaskCore);
+
+/* DELETE DAILY TASK */
+
+const deleteDailyTaskInput = z.object({
+    id: z.string(),
+});
+
+export type DailyTaskCore = z.infer<typeof dailyTaskCore>
+
+export type DeleteDailyTaskInput = z.infer<typeof deleteDailyTaskInput>
+export type CreateDailyTaskInput = z.infer<typeof createDailyTaskInput>
+export type CreateDailyTaskDetailed = z.infer<typeof createDailyTaskDetailed>
+
+export const {schemas: routineSchemas, $ref} = buildJsonSchemas({
+    createDailyTaskInput,
+    createDailyTaskResponse,
+    fetchDailyTasksResponse,
+    deleteDailyTaskInput,
+}, {$id: "RoutineSchema"})
