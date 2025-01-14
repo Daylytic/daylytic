b1f66b1aefbd933c9d234c41377a355b099883ca
iDarkQ <matikuki3@gmail.com>
2025-01-14 16:37:08 +0000

    feat(routine.service.ts): create routine service used by analytics

---
+++ apps/api/src/modules/analytics/routine/routine.service.ts
@@ -0,0 +1,21 @@
+import { prisma } from "../../../utils/prisma.js";
+import { RoutineDataCore, RoutineDataInput } from "./routine.schema.js";
+
+const initializeRoutineData = async (data: RoutineDataInput) => {
+  await prisma.routineData.create({ data: data });
+};
+
+const getRoutineData = async (data: RoutineDataInput): Promise<RoutineDataCore> => {
+  try {
+    return await prisma.routineData.findFirstOrThrow({
+      where: { analyticsId: data.analyticsId },
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const routineDataService = {
+  initializeRoutineData,
+  getRoutineData,
+};
