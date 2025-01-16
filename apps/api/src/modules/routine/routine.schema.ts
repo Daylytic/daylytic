fe7744b31ade2520884812109dcdc3efe8cf25ba
iDarkQ <matikuki3@gmail.com>
2025-01-16 08:42:42 +0000

    feat(modules): update schema, and move tasks schema into seperate file

--- apps/api/src/modules/routine/routine.schema.ts
+++ apps/api/src/modules/routine/routine.schema.ts
@@ -1,43 +1,20 @@
-import {
-  TASK_DESCRIPTION_MAX_LENGTH,
-  TASK_DESCRIPTION_MIN_LENGTH,
-  TASK_TITLE_MAX_LENGTH,
-  TASK_TITLE_MIN_LENGTH,
-} from "@daylytic/shared/constants";
 import { buildJsonSchemas } from "fastify-zod";
+import { TaskSchema } from "modules/task/task.schema.js";
 import { IdSchema } from "utils/zod.js";
 import { z } from "zod";
 
-const DescriptionSchema = z
-  .string()
-  .max(TASK_DESCRIPTION_MAX_LENGTH)
-  .min(TASK_DESCRIPTION_MIN_LENGTH)
-  .nullable()
-  .refine((value) => value === null || (value.length >= TASK_DESCRIPTION_MIN_LENGTH && value.length <= TASK_DESCRIPTION_MAX_LENGTH), {
-    message: `Description must be null or have a length between ${TASK_DESCRIPTION_MIN_LENGTH} and ${TASK_DESCRIPTION_MAX_LENGTH}.`,
-  });
-
-// Base
-const DailyTaskSchema = z.object({
-  id: IdSchema,
-  title: z.string().max(TASK_TITLE_MAX_LENGTH).min(TASK_TITLE_MIN_LENGTH),
-  description: DescriptionSchema,
-  isCompleted: z.boolean(),
-  userId: IdSchema,
-});
-
 // Create Daily Task Schemas
 
-const CreateDailyTaskInputSchema = DailyTaskSchema.pick({ title: true });
+const CreateDailyTaskInputSchema = TaskSchema.pick({ title: true, taskType: true });
 const CreateDailyTaskWithUserIdSchema = CreateDailyTaskInputSchema.extend({
   userId: IdSchema,
 });
 
 // Fetch Daily Tasks Schema
 const FetchDailyTaskInputSchema = z.object({
-  id: IdSchema,
+  userId: IdSchema,
 });
-const FetchDailyTasksResponseSchema = z.array(DailyTaskSchema);
+const FetchDailyTasksResponseSchema = z.array(TaskSchema);
 
 // Delete Daily Task Schema
 const DeleteDailyTaskInputSchema = z.object({
@@ -53,10 +30,10 @@ const ResetDailyTaskInputSchema = z.object({
 });
 
 // Update Daily Task Schema
-const UpdateDailyTaskInputSchema = DailyTaskSchema.omit({ userId: true });
-const UpdateDailyTaskWithUserIdInputSchema = DailyTaskSchema;
+const UpdateDailyTaskInputSchema = TaskSchema.omit({ userId: true });
+const UpdateDailyTaskWithUserIdInputSchema = TaskSchema;
 
-export type DailyTask = z.infer<typeof DailyTaskSchema>;
+export type DailyTask = z.infer<typeof TaskSchema>;
 export type CreateDailyTaskInputSchema = z.infer<
   typeof CreateDailyTaskInputSchema
 >;
@@ -84,7 +61,7 @@ export type FetchDailyTaskInputSchema = z.infer<
 
 export const { schemas: routineSchemas, $ref } = buildJsonSchemas(
   {
-    DailyTaskSchema,
+    TaskSchema,
     CreateDailyTaskInputSchema,
     FetchDailyTasksResponseSchema,
     DeleteDailyTaskInputSchema,
