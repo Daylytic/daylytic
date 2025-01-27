47f6dbfcfbd0d44be1cf7496944324eadfeef7fc
iDarkQ <matikuki3@gmail.com>
2025-01-27 08:25:23 +0000

    refactor(panel): replace modal for tag creation with simplified option in the ant select

--- apps/web/src/components/panel/action/routine/routine-options.tsx
+++ apps/web/src/components/panel/action/routine/routine-options.tsx
@@ -9,38 +9,86 @@ import {
   Button,
   Divider,
   Input,
+  Space,
+  InputRef,
+  theme,
+  ColorPickerProps,
+  ColorPicker,
 } from "antd";
 import dayjs from "dayjs";
 import { useDailyTasks } from "providers/daily-tasks";
-import { CreateTagModal } from "../../modal/create-tag-modal";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./routine.module.css";
 import { Tag } from "types/task";
+import { PlusOutlined } from "@ant-design/icons";
+import cuid from "cuid";
+import { blue, green, presetPalettes, red } from "@ant-design/colors";
 
 type TagRender = SelectProps["tagRender"];
+type Presets = Required<ColorPickerProps>["presets"][number];
+function genPresets(presets = presetPalettes) {
+  return Object.entries(presets).map<Presets>(([label, colors]) => ({
+    label,
+    colors,
+    key: label,
+  }));
+}
 
 export const RoutineOptions = () => {
   const { token } = theme.useToken();
   const { tasks, updateTask, selectedTask, setSelectedTask } = useDailyTasks();
   const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [color, setColor] = useState(token.colorPrimary);
+  const inputRef = useRef<InputRef>(null);
 
   const tags = [...tasks].flatMap((task) => task.tags);
 
-  const dropdownRender = (menus: React.ReactNode) => (
-    <div>
-      {menus}
-      <Divider className={styles["dropdown-divider"]} />
-      <Button
-        type="text"
-        id={styles["dropdown-button"]}
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        New Tag
-      </Button>
-    </div>
+  const presets = genPresets({
+    red,
+    green,
+    blue,
+  });
+
+  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const customPanelRender: ColorPickerProps["panelRender"] = (
+    _,
+    { components: { Picker, Presets } }
+  ) => (
+    <Row justify="space-between" wrap={false}>
+      <Col span={12}>
+        <Presets />
+      </Col>
+      <Divider type="vertical" style={{ height: "auto" }} />
+      <Col flex="auto">
+        <Picker />
+      </Col>
+    </Row>
   );
+
+  const addItem = (
+    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>
+  ) => {
+    e.preventDefault();
+    selectedTask!.tags = [
+      ...selectedTask!.tags,
+      {
+        id: cuid(),
+        name: name,
+        color: color,
+      },
+    ];
+    console.log("SELECTED ", selectedTask);
+    setSelectedTask(selectedTask);
+    updateTask(selectedTask!);
+    setName("");
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
 
   const tagRender: TagRender = (props) => {
     const { label, value, closable, onClose } = props;
@@ -69,7 +117,6 @@ export const RoutineOptions = () => {
 
   return (
     <>
-      <CreateTagModal setOpen={setOpen} open={open} />
       <Row>
         <Col span={18} push={6}>
           <DatePicker
@@ -103,18 +150,55 @@ export const RoutineOptions = () => {
               value={selectedTask!.tags.map((tag) => tag.id)}
               options={[
                 ...tags.map((tag) => {
-                  return { id: tag.id, color: tag.color, value: tag.id, name: tag.name,label: tag.name };
+                  return {
+                    id: tag.id,
+                    color: tag.color,
+                    value: tag.id,
+                    name: tag.name,
+                    label: tag.name,
+                  };
                 }),
               ]}
               onChange={(value, selectedOptions) => {
+                console.log([value, selectedOptions]);
                 if (!selectedOptions) {
                   return;
                 }
                 selectedTask!.tags = selectedOptions as Tag[];
+                console.log("SELECTED ", selectedTask);
                 setSelectedTask(selectedTask);
                 updateTask(selectedTask!);
               }}
-              dropdownRender={dropdownRender}
+              // dropdownRender={dropdownRender}
+              dropdownRender={(menu) => (
+                <>
+                  {menu}
+                  <Divider style={{ margin: "8px 0" }} />
+                  <Space style={{ padding: "0 8px 4px" }}>
+                    <Input
+                      placeholder="Tag name"
+                      ref={inputRef}
+                      value={name}
+                      onChange={onNameChange}
+                      onKeyDown={(e) => e.stopPropagation()}
+                    />
+                    <ColorPicker
+                      defaultValue={color}
+                      styles={{ popupOverlayInner: { width: 480 } }}
+                      presets={presets}
+                      panelRender={customPanelRender}
+                      onChange={(color) => setColor(color.toHexString())}
+                    />
+                    <Button
+                      type="text"
+                      icon={<PlusOutlined />}
+                      onClick={addItem}
+                    >
+                      Create
+                    </Button>
+                  </Space>
+                </>
+              )}
               placeholder="Please select"
             ></Select>
           </Flex>
