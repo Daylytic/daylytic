a30a1f68cca20614d733f059f1374e83a7456cd6
iDarkQ <matikuki3@gmail.com>
2025-02-08 14:46:43 +0000

    refactor(routine-card.tsx): make grabber part of the routine card

--- apps/web/src/components/panel/content/routine/routine-card.tsx
+++ apps/web/src/components/panel/content/routine/routine-card.tsx
@@ -3,25 +3,55 @@ import styles from "./routine.module.css";
 import { useNavigate } from "react-router";
 import { useDailyTasks } from "providers/daily-tasks";
 import { Task } from "types/task";
+import { HolderOutlined } from "@ant-design/icons";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const { Title } = Typography;
 
 export const RoutineCard = ({ item }: { item: Task }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: item.id });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : 1,
+    display: "flex",
+    alignItems: "start",
+    width: "auto !important",
+    flex: "1",
+  };
+
   const navigate = useNavigate();
   const { updateTask } = useDailyTasks();
 
   return (
-    <List.Item style={{ position: "relative", flex: "1" }}>
+    <List.Item ref={setNodeRef} style={style} className={styles.card} {...attributes}>
       <List.Item.Meta
         avatar={
-          <Checkbox
-            className={styles.checkbox}
-            defaultChecked={item.isCompleted}
-            onChange={async () => {
-              const updatedItem = { ...item, isCompleted: !item.isCompleted };
-              await updateTask(updatedItem);
-            }}
-          ></Checkbox>
+          <Flex gap="small" justify="center" align="center">
+            <HolderOutlined
+              ref={setActivatorNodeRef}
+              className={styles.grabber}
+              {...listeners}
+            />
+            <Checkbox
+              className={styles.checkbox}
+              defaultChecked={item.isCompleted}
+              onChange={async () => {
+                const updatedItem = { ...item, isCompleted: !item.isCompleted };
+                await updateTask(updatedItem);
+              }}
+            ></Checkbox>
+          </Flex>
         }
         className={styles["card-meta"]}
         description={
