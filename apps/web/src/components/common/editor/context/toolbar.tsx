06b45ad51eaa7893e7a89471c5a8d797f054fbc9
iDarkQ <matikuki3@gmail.com>
2025-02-10 18:11:58 +0000

    refactor(toolbar.tsx): get rid of unused toolbar components

--- apps/web/src/components/common/editor/context/toolbar.tsx
+++ apps/web/src/components/common/editor/context/toolbar.tsx
@@ -1,33 +1,11 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-import type {JSX} from 'react';
-
-import {ElementFormatType} from 'lexical';
 import React, {
   createContext,
   ReactNode,
   useCallback,
   useContext,
-  useEffect,
   useMemo,
   useState,
 } from 'react';
-
-export const MIN_ALLOWED_FONT_SIZE = 8;
-export const MAX_ALLOWED_FONT_SIZE = 72;
-export const DEFAULT_FONT_SIZE = 15;
-
-// eslint-disable-next-line
-const rootTypeToRootName = {
-  root: 'Root',
-  table: 'Table',
-};
 
 export const blockTypeToBlockName = {
   bullet: 'Bulleted List',
@@ -44,34 +22,17 @@ export const blockTypeToBlockName = {
   quote: 'Quote',
 };
 
-//disable eslint sorting rule for quick reference to toolbar state
 const INITIAL_TOOLBAR_STATE = {
-  bgColor: '#fff',
   blockType: 'paragraph' as keyof typeof blockTypeToBlockName,
   canRedo: false,
   canUndo: false,
   codeLanguage: '',
-  elementFormat: 'left' as ElementFormatType,
-  fontColor: '#000',
-  fontFamily: 'Arial',
-  // Current font size in px
-  fontSize: `${DEFAULT_FONT_SIZE}px`,
-  // Font size input value - for controlled input
-  fontSizeInputValue: `${DEFAULT_FONT_SIZE}`,
   isBold: false,
   isCode: false,
-  isImageCaption: false,
   isItalic: false,
   isLink: false,
-  isRTL: false,
   isStrikethrough: false,
-  isSubscript: false,
-  isSuperscript: false,
   isUnderline: false,
-  isLowercase: false,
-  isUppercase: false,
-  isCapitalize: false,
-  rootType: 'root' as keyof typeof rootTypeToRootName,
 };
 
 type ToolbarState = typeof INITIAL_TOOLBAR_STATE;
@@ -90,13 +51,14 @@ type ContextShape = {
 
 const Context = createContext<ContextShape | undefined>(undefined);
 
+interface ToolbarContextProps {
+  children: ReactNode;
+}
+
 export const ToolbarContext = ({
   children,
-}: {
-  children: ReactNode;
-}): JSX.Element => {
+}: ToolbarContextProps): JSX.Element => {
   const [toolbarState, setToolbarState] = useState(INITIAL_TOOLBAR_STATE);
-  const selectionFontSize = toolbarState.fontSize;
 
   const updateToolbarState = useCallback(
     <Key extends ToolbarStateKey>(key: Key, value: ToolbarStateValue<Key>) => {
@@ -107,10 +69,6 @@ export const ToolbarContext = ({
     },
     [],
   );
-
-  useEffect(() => {
-    updateToolbarState('fontSizeInputValue', selectionFontSize.slice(0, -2));
-  }, [selectionFontSize, updateToolbarState]);
 
   const contextValue = useMemo(() => {
     return {
