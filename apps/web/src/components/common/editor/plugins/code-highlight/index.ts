503344ba5867b89a1780272e1ec88803bf3537bf
iDarkQ <matikuki3@gmail.com>
2025-02-11 12:53:44 +0000

    refractor(code-highlight): move CodeHighlightPlugin into a seperate file, and leave exporter in index.ts

--- apps/web/src/components/common/editor/plugins/code-highlight/index.ts
+++ apps/web/src/components/common/editor/plugins/code-highlight/index.ts
@@ -1,15 +1,1 @@
-import type {JSX} from 'react';
-
-import {registerCodeHighlighting} from '@lexical/code';
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {useEffect} from 'react';
-
-export function CodeHighlightPlugin(): JSX.Element | null {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    return registerCodeHighlighting(editor);
-  }, [editor]);
-
-  return null;
-}
+export * from "./code-highlight";
