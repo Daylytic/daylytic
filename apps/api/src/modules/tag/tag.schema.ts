fdca22aebdeff58b5bc0fc78f7cd3b857798f095
iDarkQ <matikuki3@gmail.com>
2025-02-13 16:32:12 +0000

    feat(tag.schema.ts): create base, on create, on delete, and update tag schemas

---
+++ apps/api/src/modules/tag/tag.schema.ts
@@ -0,0 +1,44 @@
+import { buildJsonSchemas } from "fastify-zod";
+import { IdSchema } from "utils/zod.js";
+import { z } from "zod";
+
+export const TagNameSchema = z.string();
+
+// Base
+
+export const TagSchema = z.object({
+  id: IdSchema,
+  name: TagNameSchema,
+  color: z.string(),
+  userId: IdSchema,
+  tasks: z.array(z.string()).optional(),
+});
+
+// Create Tag Schema
+const CreateTagSchema = TagSchema.pick({color: true, name: true, userId: true});
+const CreateTagInputSchema = CreateTagSchema.omit({userId: true});
+
+// Delete Tag Schema
+const DeleteTagSchema = TagSchema.pick({id: true, userId: true});
+const DeleteTagInputSchema = DeleteTagSchema.omit({userId: true});
+
+// Update Tag Schema
+const UpdateTagSchema = TagSchema.pick({color: true, name: true, id: true});
+
+export type CreateTagSchema = z.infer<
+  typeof CreateTagSchema
+>;
+export type DeleteTagSchema = z.infer<typeof DeleteTagSchema>;
+export type UpdateTagSchema = z.infer<typeof UpdateTagSchema>;
+
+
+
+export const { schemas: tagSchemas, $ref } = buildJsonSchemas(
+  {
+    TagSchema,
+    CreateTagInputSchema,
+    DeleteTagInputSchema,
+
+  },
+  { $id: "TagSchemas" }
+);
