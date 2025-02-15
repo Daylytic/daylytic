a01d7b917b03e0c36ed0a0bfdb9b431797f94dc0
iDarkQ <matikuki3@gmail.com>
2025-02-15 07:28:38 +0000

    feat(tag.service.ts): create fetchTags and fetchTagsWithIds functions

--- apps/api/src/modules/tag/tag.service.ts
+++ apps/api/src/modules/tag/tag.service.ts
@@ -1,8 +1,30 @@
 import { prisma } from "utils/prisma.js";
-import { CreateTagSchema, DeleteTagSchema, UpdateTagSchema } from "./index.js";
+import {
+  CreateTagSchema,
+  DeleteTagSchema,
+  FetchTagsInputSchema,
+  FetchTagsResponseSchema,
+  FetchTagsWithIdsSchema,
+  TagSchema,
+  UpdateTagInputSchema,
+  UpdateTagSchema,
+} from "./index.js";
 import { RequestError } from "utils/error.js";
 
-const createTag = async (data: CreateTagSchema) => {
+const fetchTags = async (
+  data: FetchTagsInputSchema
+): Promise<FetchTagsResponseSchema> => {
+  try {
+    return await prisma.tag.findMany({ where: { userId: data.userId } });
+  } catch (err) {
+    throw new RequestError(
+      "Could not find tags for the specific user",
+      404,
+      err
+    );
+  }
+};
+
   try {
     await prisma.tag.create({ data: data });
   } catch (err) {
@@ -29,7 +51,36 @@ const updateTag = async (data: UpdateTagSchema) => {
   }
 };
 
+const fetchTagsWithIds = async (data: FetchTagsWithIdsSchema): Promise<TagSchema[]> => {
+  try {
+    const foundTags: TagSchema[] = [];
+    for (const id of data) {
+
+      const tag = await prisma.tag.findFirst({
+        where: {
+          id: id,
+        },
+      });
+
+      console.log(`Found tag: ${tag}`)
+
+      if (!tag) continue;
+
+      foundTags.push(tag);
+    }
+    return foundTags;
+  } catch (err) {
+    throw new RequestError(
+      "Problem occured when fetching tags with specific id",
+      500,
+      err
+    );
+  }
+};
+
 export const tagService = {
+  fetchTags,
+  fetchTagsWithIds,
     createTag,
     deleteTag,
     updateTag,
