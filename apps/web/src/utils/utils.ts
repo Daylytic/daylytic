3cac1faeec79209bdc5c3a4ec9284508211a85b3
iDarkQ <matikuki3@gmail.com>
2025-02-19 14:49:47 +0000

    feat(utils.ts): create formatDate function

--- apps/web/src/utils/utils.ts
+++ apps/web/src/utils/utils.ts
@@ -1,5 +1,30 @@
+import { DateTime } from "luxon";
+
 export function timeout(delay: number) {
   return new Promise((res) => setTimeout(res, delay));
 }
 
 export const timeFormat = 'HH:mm';
+const getOrdinalSuffix = (day: number): string => {
+  if (day > 3 && day < 21) return 'th'; 
+  switch (day % 10) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
+}
+
+export const formatDate = (dateStr: string): string  =>{
+  const dt = DateTime.fromISO(dateStr, { zone: 'utc' });
+  
+  const time = dt.toFormat('HH:mm');
+
+  const day = dt.day;
+  const dayWithSuffix = `${day}${getOrdinalSuffix(day)}`;
+  
+  const month = dt.toFormat('LLLL');
+  const year = dt.toFormat('yyyy');
+  
+  return `${time} ${dayWithSuffix} ${month} ${year}`;
+}
