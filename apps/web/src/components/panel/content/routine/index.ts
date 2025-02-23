f8a0e653fe04c055633e9546124ffab1e1736602
iDarkQ <matikuki3@gmail.com>
2025-02-23 15:40:44 +0000

    feat(index.ts): export all of the files from the same folder

--- apps/web/src/components/panel/content/routine/index.ts
+++ apps/web/src/components/panel/content/routine/index.ts
@@ -1,1 +1,7 @@
 export * from "./routine";
+export * from "./routine-header";
+export * from "./routine-list";
+export * from "./routine-task-input";
+export * from "./use-task-input.tsx";
+export * from "./routine-card";
+export { default as styles } from "./routine.module.css";
