039286528dd1763db688933673e0241f8e25079a
iDarkQ <matikuki3@gmail.com>
2025-02-23 15:48:14 +0000

    refactor(routine-header.tsx): begin to use useHeader hook

--- apps/web/src/components/panel/action/routine/routine-header.tsx
+++ apps/web/src/components/panel/action/routine/routine-header.tsx
@@ -1,84 +1,17 @@
 import { EditOutlined, InfoCircleOutlined } from "@ant-design/icons";
-import { Flex, Typography, Popover, Descriptions, Button, DescriptionsProps, Skeleton, Space } from "antd";
+import { Flex, Typography, Popover, Descriptions, Button, Skeleton } from "antd";
 import clsx from "clsx";
 import { useDailyTasks } from "providers/daily-tasks";
-import styles from "./routine.module.css";
-import { useState } from "react";
-import { formatDate } from "utils/utils";
+import { RoutineHeaderSkeleton, styles, useHeader } from ".";
 
 export const RoutineHeader = () => {
   const { selectedTask, updateTask } = useDailyTasks();
 
-  // HACK: Prevents counting characters and words every time user types something.
-  // Instead, we only perform calculations when user clicks on the buttons with popover.
-  const [counts, setCounts] = useState<[number, number]>();
-
-  // eslint-disable-next-line
-  const countCharactersAndWords = (node: any): [number, number] => {
-    let charCount = 0;
-    let wordCount = 0;
-
-    if (node === undefined) {
-      return [wordCount, charCount];
-    }
-
-    if (node.text && typeof node.text === "string") {
-      charCount += node.text.length;
-      const words = node.text
-        .trim()
-        .split(/\s+/)
-        .filter((word) => word.length > 0);
-      wordCount += words.length;
-    }
-
-    if (node.children && Array.isArray(node.children)) {
-      for (const child of node.children) {
-        const [childChars, childWords] = countCharactersAndWords(child);
-        charCount += childChars;
-        wordCount += childWords;
-      }
-    }
-
-    return [wordCount, charCount];
-  };
-
   if (selectedTask!.current === undefined) {
-    return (
-      <Flex gap={"small"}>
-        <Skeleton.Input size="large" active block={true} />
-        <Skeleton.Avatar active shape={"circle"} />
-      </Flex>
-    );
+    return <RoutineHeaderSkeleton />;
   }
 
-  const handleOpenDetails = () => {
-    const rootNode = selectedTask!.current!.content!["root"];
-    const [totalWords, totalChars] = countCharactersAndWords(rootNode);
-    setCounts([totalWords, totalChars]);
-  };
-
-  const items: DescriptionsProps["items"] = [
-    {
-      key: "1",
-      label: "Words",
-      children: counts ? counts[0] : "...",
-    },
-    {
-      key: "2",
-      label: "Characters",
-      children: counts ? counts[1] : "...",
-    },
-    {
-      key: "3",
-      label: "Created",
-      children: formatDate(selectedTask!.current!.createdAt),
-    },
-    {
-      key: "4",
-      label: "Updated",
-      children: formatDate(selectedTask!.current!.updatedAt),
-    },
-  ];
+  const { menuItems, handleOpenDetails } = useHeader({ selectedTask });
 
   return (
     <Flex className={clsx("ant-typography")} justify="space-between" align="center">
