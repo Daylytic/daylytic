e275c36109618b47d8418cd5b9db105ee66a1c0c
iDarkQ <matikuki3@gmail.com>
2025-02-24 07:46:49 +0000

    feat(menu): create and display tags in the menu

--- apps/web/src/components/panel/menu/menu.tsx
+++ apps/web/src/components/panel/menu/menu.tsx
@@ -3,11 +3,13 @@ import { General } from "./general";
 import { Goals } from "./goals";
 import styles from "./menu.module.css";
 import { Header } from "./header";
+import { Tags } from "./tags";
 
 export const Menu = () => (
   <Sider width={400} id={styles.sider}>
     <Header />
     <General />
+    <Tags />
     <Goals />
   </Sider>
 );
