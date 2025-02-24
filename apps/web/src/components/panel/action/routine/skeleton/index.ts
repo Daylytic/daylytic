cb4e03e27d3f5e946617d4b1ad5947f17fb075d6
iDarkQ <matikuki3@gmail.com>
2025-02-24 07:43:21 +0000

    fix(skeleton): scheme not matching the actual content of routine list

--- apps/web/src/components/panel/action/routine/skeleton/index.ts
+++ apps/web/src/components/panel/action/routine/skeleton/index.ts
@@ -1,3 +1,5 @@
 export * from "./routine-header-skeleton.tsx";
 export * from "./routine-editor-skeleton.tsx";
 export * from "./routine-settings-skeleton.tsx";
+
+export {default as styles} from "./skeleton.module.css";
