9bb65476804550f6181bd05141edfbf0f245ec59
iDarkQ <matikuki3@gmail.com>
2025-02-25 08:49:23 +0000

    refactor(routine): replace usePanel hook with useParams to obtain tagId & add sider

--- apps/web/src/components/panel/action/routine/routine.tsx
+++ apps/web/src/components/panel/action/routine/routine.tsx
@@ -1,21 +1,20 @@
 import { Flex } from "antd";
 import { useEffect } from "react";
 import { useDailyTasks } from "providers/daily-tasks";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { RoutineHeader, RoutineSettings, styles } from ".";
 import { RoutineEditor } from "./routine-editor";
+import Sider from "antd/es/layout/Sider";
 
-interface RoutineProps {
-  id: string;
-}
+export const Routine = () => {
+  const { taskId } = useParams();
 
-export const Routine = ({ id }: RoutineProps) => {
   const { tasks, selectedTask, fetched } = useDailyTasks();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (selectedTask.current === undefined) {
-      const task = tasks.find((task) => task.id === id);
+      const task = tasks.find((task) => task.id === taskId);
       selectedTask.current = task;
 
       // If tasks fetched, and selectedTask is not selected, navigate back to /panel/routine
@@ -26,10 +25,12 @@ export const Routine = ({ id }: RoutineProps) => {
   });
 
   return (
-    <Flex vertical id={styles["wrapper"]} gap={selectedTask.current === undefined ? "small" : 0}>
-      <RoutineHeader />
-      <RoutineSettings />
-      <RoutineEditor />
-    </Flex>
+    <Sider width={500} id={styles.sider}>
+      <Flex vertical id={styles["wrapper"]} gap={selectedTask.current === undefined ? "small" : 0}>
+        <RoutineHeader />
+        <RoutineSettings />
+        <RoutineEditor />
+      </Flex>
+    </Sider>
   );
 };
