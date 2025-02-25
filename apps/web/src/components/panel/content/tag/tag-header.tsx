d9dc6c5c9ee05091ba3ed7d6464bad8d048963e9
iDarkQ <matikuki3@gmail.com>
2025-02-25 08:48:57 +0000

    refactor(tag-header.tsx): replace usePanel hook with useParams to obtain tagId

--- apps/web/src/components/panel/content/tag/tag-header.tsx
+++ apps/web/src/components/panel/content/tag/tag-header.tsx
@@ -1,24 +1,24 @@
 import { Flex, Typography } from "antd";
 import { Tag } from "components/common/tag";
-import { usePanel } from "hooks/use-panel";
 import { useTags } from "providers/tag";
+import { useParams } from "react-router";
 
 const { Title } = Typography;
 
 export const TagHeader = () => {
-  const { getContent } = usePanel();
+  const { tagId } = useParams();
   const { tags } = useTags();
 
-  const selectedTag = tags.find((tag) => tag.id === getContent());
+  const selectedTag = tags.find((tag) => tag.id === tagId);
 
-  if(!selectedTag) {
+  if (!selectedTag) {
     return <></>;
   }
 
   return (
     <Flex align="center" gap="small">
       <Title level={1}>Tag</Title>
-      <Tag tag={selectedTag}/>
+      <Tag tag={selectedTag} />
     </Flex>
   );
 };
