02c335f5d701b7fe25a32ab49217f62822aad61d
iDarkQ <matikuki3@gmail.com>
2025-02-25 16:34:32 +0000

    refactor: move config for ant-design into a seperate file

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -8,40 +8,7 @@ import { LayoutRoutine, LayoutTag, LayoutPanel } from "components/layout";
 import { config } from "config";
 
 export const App = () => (
-  <ConfigProvider
-    theme={{
-      hashed: false,
-      cssVar: true,
-      token: {
-        // Seed Token
-        colorPrimary: "#6247aa",
-        borderRadius: 5,
-        fontFamily: "Poppins",
-        colorBgLayout: "#f5f5f5",
-        colorWhite: "#fafafa",
-      },
-      components: {
-        Layout: {
-          headerBg: "#f5f5f5",
-          headerColor: "#f5f5f5",
-          headerPadding: 0,
-          colorBgContainer: "#fafafa",
-          bodyBg: "#fafafa",
-          siderBg: "#f5f5f5",
-        },
-        Menu: {
-          popupBg: "#f5f5f5",
-          itemBg: "#f5f5f5",
-          activeBarBorderWidth: 0,
-          itemMarginInline: 0,
-        },
-        List: {
-          metaMarginBottom: 0,
-          itemPadding: "4px 0",
-        },
-      },
-    }}
-  >
+  <ConfigProvider theme={config}>
     <BrowserRouter>
       <ScrollToHash />
       <Routes>
---
+++ apps/web/src/config.ts
@@ -0,0 +1,32 @@
+export const config = {
+  hashed: false,
+  cssVar: true,
+  token: {
+    // Seed Token
+    colorPrimary: "#6247aa",
+    borderRadius: 5,
+    fontFamily: "Poppins",
+    colorBgLayout: "#f5f5f5",
+    colorWhite: "#fafafa",
+  },
+  components: {
+    Layout: {
+      headerBg: "#f5f5f5",
+      headerColor: "#f5f5f5",
+      headerPadding: 0,
+      colorBgContainer: "#fafafa",
+      bodyBg: "#fafafa",
+      siderBg: "#f5f5f5",
+    },
+    Menu: {
+      popupBg: "#f5f5f5",
+      itemBg: "#f5f5f5",
+      activeBarBorderWidth: 0,
+      itemMarginInline: 0,
+    },
+    List: {
+      metaMarginBottom: 0,
+      itemPadding: "4px 0",
+    },
+  },
+};
