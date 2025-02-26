e3f95de18f215815d670447060aec16b2e3479f8
iDarkQ <matikuki3@gmail.com>
2025-02-26 14:48:38 +0000

    feat(goal.schema.ts): create schemas for goal endpoint

--- apps/api/src/modules/goal/goal.schema.ts
+++ apps/api/src/modules/goal/goal.schema.ts
@@ -8,17 +8,31 @@ const GoalSchema = z.object({
     id: IdSchema,
     title: z.string(),
     description: z.string(),
-    projectIds: z.array(z.string()),
+    userId: IdSchema,
 });
 
 const CreateGoalInputSchema = GoalSchema.pick({title: true, description: true});
+const CreateGoalSchema = GoalSchema.pick({title: true, description: true, userId: true});
+const FetchGoalsSchema = GoalSchema.pick({userId: true});
+const DeleteGoalInputSchema = GoalSchema.pick({id: true});
+const DeleteGoalSchema = GoalSchema.pick({id: true, userId: true});
+const UpdateGoalSchema = GoalSchema;
+
+export type GoalSchema = z.infer<typeof GoalSchema>;
 
 export type CreateGoalInputSchema = z.infer<typeof CreateGoalInputSchema>;
+export type CreateGoalSchema = z.infer<typeof CreateGoalSchema>;
+export type FetchGoalsSchema = z.infer<typeof FetchGoalsSchema>;
+export type DeleteGoalInputSchema = z.infer<typeof DeleteGoalInputSchema>;
+export type DeleteGoalSchema = z.infer<typeof DeleteGoalSchema>;
+export type UpdateGoalSchema = z.infer<typeof UpdateGoalSchema>;
 
 export const { schemas: goalSchemas, $ref } = buildJsonSchemas(
     {
       GoalSchema,
       CreateGoalInputSchema,
+      DeleteGoalInputSchema,
+      UpdateGoalSchema,
     },
     { $id: "GoalSchemas" }
   );
