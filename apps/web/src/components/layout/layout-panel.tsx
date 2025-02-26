3e1141bdbc8623718a04b03c890f1e3adfb294aa
iDarkQ <matikuki3@gmail.com>
2025-02-26 07:54:39 +0000

    fix(layout-panel.tsx): bad import

--- apps/web/src/components/layout/layout-panel.tsx
+++ apps/web/src/components/layout/layout-panel.tsx
@@ -1,9 +1,10 @@
-import { Layout as AntLayout, Menu } from "antd";
-import styles from "./layout.module.css";
+import { Layout as AntLayout } from "antd";
+import { styles } from ".";
 import { useCookies } from "react-cookie";
 import { DailyTasksProvider } from "providers/daily-tasks";
 import { TagProvider } from "providers/tag";
 import { Outlet } from "react-router";
+import { Menu } from "components/panel/menu";
 
 export const LayoutPanel = () => {
   const [cookies] = useCookies(["token"]);
