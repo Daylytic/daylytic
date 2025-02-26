2c3c98c9fe5357f4ae5409571fdd5691f2e10614
iDarkQ <matikuki3@gmail.com>
2025-02-26 14:47:34 +0000

    refactor(goal): finish create. delete, fetch, and update goal

--- apps/api/src/modules/goal/goal.controller.ts
+++ apps/api/src/modules/goal/goal.controller.ts
@@ -1,24 +1,59 @@
-import { FastifyReply, FastifyRequest } from "fastify"
-import { handleControllerError } from "utils/error.js"
+import { FastifyReply, FastifyRequest } from "fastify";
+import { handleControllerError, RequestError } from "utils/error.js";
+import { goalService } from "./goal.service.js";
+import {
+  CreateGoalInputSchema,
+  DeleteGoalInputSchema,
+  GoalSchema,
+} from "./goal.schema.js";
 
 const createGoal = async (req: FastifyRequest, rep: FastifyReply) => {
-    try {
-        
-    }catch(err) {
-        handleControllerError(err, rep);
+  try {
+    const userId = req.user!.id;
+    const { title, description } = req.body as CreateGoalInputSchema;
+    return await goalService.createGoal({ userId, title, description });
+  } catch (err) {
+    handleControllerError(err, rep);
+  }
+};
+
+const fetchGoals = async (req: FastifyRequest, rep: FastifyReply) => {
+  try {
+    const userId = req.user!.id;
+    return await goalService.fetchGoals({ userId });
+  } catch (err) {
+    handleControllerError(err, rep);
+  }
+};
+
+const deleteGoal = async (req: FastifyRequest, rep: FastifyReply) => {
+  try {
+    const userId = req.user!.id;
+    const { id } = req.body as DeleteGoalInputSchema;
+    return await goalService.deleteGoal({ userId, id });
+  } catch (err) {
+    handleControllerError(err, rep);
+  }
+};
+
+const updateGoal = async (req: FastifyRequest, rep: FastifyReply) => {
+  try {
+    const userId = req.user!.id;
+    const task = req.body as GoalSchema;
+
+    if (task.userId !== userId) {
+      throw new RequestError("You do not have access to this goal.", 403, null);
     }
-}
 
-const deleteGoal = async () => {
-
-}
-
-const updateGoal = async () => {
-
-}
+    return await goalService.updateGoal(task);
+  } catch (err) {
+    handleControllerError(err, rep);
+  }
+};
 
 export const goalController = {
-    createGoal,
-    deleteGoal,
-    updateGoal,
-}
+  createGoal,
+  fetchGoals,
+  deleteGoal,
+  updateGoal,
+};
--- apps/api/src/modules/goal/goal.routes.ts
+++ apps/api/src/modules/goal/goal.routes.ts
@@ -1,23 +1,72 @@
 import { FastifyPluginAsync } from "fastify";
 import { goalController } from "./goal.controller.js";
 import { $ref } from "./goal.schema.js";
-import {$ref as $refAuth} from "modules/auth/auth.schema.js";
+import { $ref as $refAuth } from "modules/auth/auth.schema.js";
+import { authController } from "modules/auth/auth.controller.js";
 
 export const goalHandler: FastifyPluginAsync = async (server, _) => {
-    // Create Goal
-    server.route({
-        url: "/",
-        method: "POST",
-        preHandler: [],
-        handler: goalController.createGoal,
-        schema: {
-            description: "Create goal",
-            tags: ["goal"],
-            body: $ref("CreateGoalInputSchema"),
-            headers: $refAuth("HeaderBearerSchema"),
-            response: {
-                201: $ref("GoalSchema")
-            }
-        }
-    });
-}
+  // Create Goal
+  server.route({
+    url: "/",
+    method: "POST",
+    preHandler: [authController.authenticate],
+    handler: goalController.createGoal,
+    schema: {
+      description: "Create goal",
+      tags: ["goal"],
+      body: $ref("CreateGoalInputSchema"),
+      headers: $refAuth("HeaderBearerSchema"),
+      response: {
+        201: $ref("GoalSchema"),
+      },
+    },
+  });
+
+  // Fetch
+  server.route({
+    url: "/",
+    method: "GET",
+    preHandler: [authController.authenticate],
+    handler: goalController.fetchGoals,
+    schema: {
+      description: "Fetch goals",
+      tags: ["goal"],
+      headers: $refAuth("HeaderBearerSchema"),
+      response: {
+        201: $ref("GoalSchema"),
+      },
+    },
+  });
+
+  // Delete
+  server.route({
+    url: "/",
+    method: "DELETE",
+    preHandler: [authController.authenticate],
+    handler: goalController.fetchGoals,
+    schema: {
+      description: "Delete goal",
+      tags: ["goal"],
+      headers: $refAuth("HeaderBearerSchema"),
+      204: {
+        description: "Succesfully deleted goal",
+      },
+    },
+  });
+
+  // UPDATE
+  server.route({
+    url: "/",
+    method: "PUT",
+    preHandler: [authController.authenticate],
+    handler: goalController.updateGoal,
+    schema: {
+      description: "Update goal",
+      tags: ["goal"],
+      headers: $refAuth("HeaderBearerSchema"),
+      response: {
+        201: $ref("GoalSchema"),
+      },
+    },
+  });
+};
