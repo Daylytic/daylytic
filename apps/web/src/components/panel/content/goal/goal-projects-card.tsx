a6364828df418d18785a996cc2df5c7987a5a113
iDarkQ <matikuki3@gmail.com>
2025-02-27 07:44:09 +0000

    refactor(goal-projects-card.tsx): use tasklist inside card

--- apps/web/src/components/panel/content/goal/goal-projects-card.tsx
+++ apps/web/src/components/panel/content/goal/goal-projects-card.tsx
@@ -1,36 +1,48 @@
 import { Card } from "antd";
 import { styles } from ".";
-import { TaskCard } from "components/common/task";
+import { TaskCard, TaskList } from "components/common/task";
+import { Task } from "types/task";
+import { ReactNode } from "react";
 
 export const GoalProjectsCard = ({ contentLines }: { contentLines: number }) => {
+  const tasks = [...Array(contentLines)].map((_, i) => ({
+    id: i.toString(),
+    position: 0,
+    taskType: "TODOLIST",
+    priority: null,
+    title: `Task Cardd ${i}`,
+    content: {},
+    isCompleted: true,
+    createdAt: "",
+    updatedAt: "",
+    deadline: null,
+    userId: "",
+    tagIds: [],
+  })) as Task[];
+
   return (
     <Card title="Default Project" extra={<a href="#">More</a>} className={styles.card}>
-      {[...Array(contentLines)].map((_, i) => (
-        <TaskCard
-          orderable={false}
-          item={{
-            id: i.toString(),
-            position: 0,
-            taskType: "TODOLIST",
-            priority: null,
-            title: `Task Card ${i}`,
-            content: {},
-            isCompleted: true,
-            createdAt: "",
-            updatedAt: "",
-            deadline: null,
-            userId: "",
-            tagIds: [],
-          }}
-          onClick={function (): void {
-            throw new Error("Function not implemented.");
-          }}
-          onCheckboxChange={function (): Promise<void> {
-            throw new Error("Function not implemented.");
-          }}
-          tags={[]}
-        />
-      ))}
+      <TaskList
+        orderable={true}
+        fetched={true}
+        tasks={tasks}
+        updateTask={(_) => {}}
+        renderItem={(item: Task): ReactNode => {
+          return (
+            <TaskCard
+              orderable={false}
+              item={item}
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+              onCheckboxChange={function (): Promise<void> {
+                throw new Error("Function not implemented.");
+              }}
+              tags={[]}
+            />
+          );
+        }}
+      />
     </Card>
   );
 };
