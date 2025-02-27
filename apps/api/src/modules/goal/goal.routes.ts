9e20817344696703f1b3a437dc8a53e2d6f2f321
iDarkQ <matikuki3@gmail.com>
2025-02-27 16:34:33 +0000

    refactor(goal.routes.ts): write better comments and descriptions for goal endpoints

--- apps/api/src/modules/goal/goal.routes.ts
+++ apps/api/src/modules/goal/goal.routes.ts
@@ -5,7 +5,7 @@ import { $ref as $refAuth } from "modules/auth/auth.schema.js";
 import { authController } from "modules/auth/auth.controller.js";
 
 export const goalHandler: FastifyPluginAsync = async (server, _) => {
-  // Create Goal
+  // Create goal POST /user/goal
   server.route({
     url: "/",
     method: "POST",
@@ -22,7 +22,7 @@ export const goalHandler: FastifyPluginAsync = async (server, _) => {
     },
   });
 
-  // Fetch
+  // Fetch all goals GET /user/goal
   server.route({
     url: "/",
     method: "GET",
@@ -38,14 +38,14 @@ export const goalHandler: FastifyPluginAsync = async (server, _) => {
     },
   });
 
-  // Delete
+  // Delete specific goal DELETE /user/goal/:id
   server.route({
     url: "/",
     method: "DELETE",
     preHandler: [authController.authenticate],
     handler: goalController.fetchGoals,
     schema: {
-      description: "Delete goal",
+      description: "Delete a specific goal",
       tags: ["goal"],
       headers: $refAuth("HeaderBearerSchema"),
       204: {
@@ -54,7 +54,7 @@ export const goalHandler: FastifyPluginAsync = async (server, _) => {
     },
   });
 
-  // UPDATE
+  // Update specific project PUT /user/goal
   server.route({
     url: "/",
     method: "PUT",
