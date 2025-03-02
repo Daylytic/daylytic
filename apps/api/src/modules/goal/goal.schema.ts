26aa5df8839bfa6b84c0f841c616bbe612bb323c
iDarkQ <matikuki3@gmail.com>
2025-03-02 10:20:39 +0000

    feat(goal.schema.ts): create all of the naccessary schemas for routes and services

--- apps/api/src/modules/goal/goal.schema.ts
+++ apps/api/src/modules/goal/goal.schema.ts
@@ -1,23 +1,27 @@
 import { buildJsonSchemas } from "fastify-zod";
+import { ProjectSchema } from "modules/project/project.schema.js";
+import { TaskSchema } from "modules/task/index.js";
 import { IdSchema } from "utils/zod.js";
 import { z } from "zod";
 
 // Base
 
 const GoalSchema = z.object({
-    id: IdSchema,
-    title: z.string(),
-    description: z.string(),
-    userId: IdSchema,
+  id: IdSchema,
+  title: z.string(),
+  description: z.string(),
+  userId: IdSchema,
 });
 
 const AuthenticateGoalParamsInput = z.object({ goalId: IdSchema });
-const CreateGoalInputSchema = GoalSchema.pick({title: true, description: true});
-const CreateGoalSchema = GoalSchema.pick({title: true, description: true, userId: true});
-const FetchGoalsSchema = GoalSchema.pick({userId: true});
-const FetchGoalWithIdSchema = GoalSchema.pick({id: true, userId: true});
-const DeleteGoalInputSchema = GoalSchema.pick({id: true});
-const DeleteGoalSchema = GoalSchema.pick({id: true, userId: true});
+const CreateGoalInputSchema = GoalSchema.pick({ title: true, description: true });
+const CreateGoalSchema = GoalSchema.pick({ title: true, description: true, userId: true });
+const FetchGoalsSchema = GoalSchema.pick({ userId: true });
+const FetchGoalWithIdSchema = GoalSchema.pick({ id: true, userId: true });
+const FetchGoalsResponseSchema = z.array(GoalSchema);
+const FetchAllResponseSchema = z.array(GoalSchema.extend({ projects: z.array(ProjectSchema.extend({ tasks: z.array(TaskSchema) })) }));
+const DeleteGoalInputSchema = GoalSchema.pick({ id: true });
+const DeleteGoalSchema = GoalSchema.pick({ id: true, userId: true });
 const UpdateGoalSchema = GoalSchema;
 
 export type GoalSchema = z.infer<typeof GoalSchema>;
@@ -27,17 +31,19 @@ export type CreateGoalInputSchema = z.infer<typeof CreateGoalInputSchema>;
 export type CreateGoalSchema = z.infer<typeof CreateGoalSchema>;
 export type FetchGoalsSchema = z.infer<typeof FetchGoalsSchema>;
 export type FetchGoalWithIdSchema = z.infer<typeof FetchGoalWithIdSchema>;
+export type FetchGoalsResponseSchema = z.infer<typeof FetchGoalsResponseSchema>;
 export type DeleteGoalInputSchema = z.infer<typeof DeleteGoalInputSchema>;
 export type DeleteGoalSchema = z.infer<typeof DeleteGoalSchema>;
 export type UpdateGoalSchema = z.infer<typeof UpdateGoalSchema>;
 
 export const { schemas: goalSchemas, $ref } = buildJsonSchemas(
-    {
-      GoalSchema,
-      CreateGoalInputSchema,
-      DeleteGoalInputSchema,
-      UpdateGoalSchema,
-    },
-    { $id: "GoalSchemas" }
-  );
-  
+  {
+    GoalSchema,
+    CreateGoalInputSchema,
+    DeleteGoalInputSchema,
+    UpdateGoalSchema,
+    FetchGoalsResponseSchema,
+    FetchAllResponseSchema,
+  },
+  { $id: "GoalSchemas" }
+);
