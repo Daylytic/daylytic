4a0911e383a3d60ba4fe46c377ddcd46be3a30ed
iDarkQ <matikuki3@gmail.com>
2025-03-04 12:55:06 +0000

    refactor(goal-header.tsx): add description to goal header

--- apps/web/src/components/panel/content/goal/goal-header.tsx
+++ apps/web/src/components/panel/content/goal/goal-header.tsx
@@ -1,12 +1,21 @@
 import { Flex, Typography } from "antd";
-
+import { styles } from ".";
+import { useParams } from "react-router";
+import { useGoal } from "providers/goal";
 const { Title } = Typography;
 
 export const GoalHeader = () => {
+  const { goalId } = useParams();
+  const { goals } = useGoal();
 
+  const selectedGoal = goals.find((goal) => goal.id === goalId);
+  
   return (
-    <Flex>
-      <Title level={1}>Goal</Title>
+    <Flex vertical>
+      <Title level={1}>{selectedGoal?.title}</Title>
+      <Typography.Paragraph className={styles["header-description"]}>
+        {selectedGoal?.description}
+      </Typography.Paragraph>
     </Flex>
   );
 };
