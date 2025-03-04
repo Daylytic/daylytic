16c5cea622be95299c05208ee2da3e45e41a5f66
iDarkQ <matikuki3@gmail.com>
2025-03-04 12:56:15 +0000

    refactor(goal.tsx): set selectedGoal in provider, and if params are not provided to verify selected goal, redirect to dashboard

--- apps/web/src/components/panel/content/goal/goal.tsx
+++ apps/web/src/components/panel/content/goal/goal.tsx
@@ -1,8 +1,22 @@
 import { Content } from "antd/es/layout/layout";
 import { GoalHeader, styles } from ".";
 import { GoalProjectsList } from "components/panel/content/goal/goal-projects-list";
+import { redirect, useParams } from "react-router";
+import { useGoal } from "providers/goal";
+import { Routes } from "utils/routes";
 
 export const Goals = () => {
+  const { goalId } = useParams();
+  const { goals, selectedGoal } = useGoal();
+
+  if(!goalId) {
+    redirect(Routes.PANEL_DASHBOARD);
+  }
+
+  if (!selectedGoal.current) {
+    selectedGoal.current = goals.find((goal) => goal.id === goalId);
+  }
+
   return (
     <Content id={styles.content}>
       <GoalHeader />
