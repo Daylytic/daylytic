fced804c228775adc7af36735b25fdbe35395d5b
iDarkQ <matikuki3@gmail.com>
2025-03-05 13:12:24 +0000

    feat(app.tsx): create goal route with sub route

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import { ScrollToHash } from "components/common/scroll-to-hash";
 import { Routine as RoutineAction } from "components/panel/action/routine";
 import { Tag as TagAction } from "components/panel/action/tag";
+import { Goal as GoalAction } from "components/panel/action/goal";
 import { LayoutRoutine, LayoutTag, LayoutPanel, LayoutHome } from "components/layout";
 import { config } from "config";
 import { LayoutGoal } from "components/layout/layout-goal";
@@ -14,8 +15,8 @@ export const App = () => (
       <Routes>
         <Route index element={<LayoutHome />} />
         <Route path="/panel/*" element={<LayoutPanel />}>
-          <Route path="dashboard" element={<LayoutGoal />}>
-            <Route path=":goalId" element={<></>} />
+          <Route path="goal/:goalId" element={<LayoutGoal />}>
+            <Route path=":taskId" element={<GoalAction />} />
           </Route>
           <Route path="routine" element={<LayoutRoutine />}>
             <Route path=":taskId" element={<RoutineAction />} />
