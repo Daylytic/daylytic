5bc791bdaa9bd36a6ccf6711f60581fafef5150e
iDarkQ <matikuki3@gmail.com>
2025-03-05 13:12:51 +0000

    feat(routes.ts): create getGoalRoute, and PANEL_GOAL constant

--- apps/web/src/utils/routes.ts
+++ apps/web/src/utils/routes.ts
@@ -2,8 +2,12 @@ export enum Routes {
   PANEL_DASHBOARD = "/panel/dashboard",
   PANEL_ROUTINE = "/panel/routine",
   PANEL_TAG = "/panel/tag",
+  PANEL_GOAL = "/panel/goal"
 }
 
 export const getTaskRoute = (taskId?: string) => `${Routes.PANEL_ROUTINE}/${taskId}`;
 export const getTagRoute = (tagId: string, taskId?: string) =>
   `${Routes.PANEL_TAG}/${tagId}${taskId ? `/${taskId}` : ``}`;
+
+export const getGoalRoute = (goalId: string, taskId?: string) =>
+  `${Routes.PANEL_GOAL}/${goalId}${taskId ? `/${taskId}` : ``}`;
