638abde5a389c516e7d27aca263337ccc96aeee7
iDarkQ <matikuki3@gmail.com>
2025-03-05 13:18:23 +0000

    refactor(layout-panel.tsx): wrap everything in goal provider

--- apps/web/src/components/layout/layout-panel.tsx
+++ apps/web/src/components/layout/layout-panel.tsx
@@ -5,17 +5,20 @@ import { DailyTasksProvider } from "providers/daily-tasks";
 import { TagProvider } from "providers/tag";
 import { Outlet } from "react-router";
 import { Menu } from "components/panel/menu";
+import { GoalProvider } from "providers/goal";
 
 export const LayoutPanel = () => {
   const [cookies] = useCookies(["token"]);
   return (
-    <DailyTasksProvider token={cookies.token}>
-      <TagProvider token={cookies.token}>
-        <AntLayout id={styles.panel}>
-          <Menu />
-          <Outlet />
-        </AntLayout>
-      </TagProvider>
-    </DailyTasksProvider>
+    <GoalProvider token={cookies.token}>
+      <DailyTasksProvider token={cookies.token}>
+        <TagProvider token={cookies.token}>
+          <AntLayout id={styles.panel}>
+            <Menu />
+            <Outlet />
+          </AntLayout>
+        </TagProvider>
+      </DailyTasksProvider>
+    </GoalProvider>
   );
 };
