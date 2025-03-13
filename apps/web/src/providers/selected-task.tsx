632ca362ae02b53035c23fb7dc9e1a814c71802b
iDarkQ <matikuki3@gmail.com>
2025-03-13 16:03:23 +0000

    feat(selected-task.tsx): create selecteed task provider for updating action when switching between tasks. The reason for keeping it here and not task.tsx, is performence.

---
+++ apps/web/src/providers/selected-task.tsx
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState } from "react";
+import { Task } from "types/task";
+
+interface SelectedTaskContextType {
+  selectedTask: Task | undefined;
+  setSelectedTask: (task: Task | undefined) => void;
+}
+
+const SelectedTaskContext = createContext<SelectedTaskContextType | undefined>(undefined);
+
+export const SelectedTaskProvider = ({ children }) => {
+  const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
+
+  return (
+    <SelectedTaskContext.Provider value={{ selectedTask, setSelectedTask }}>
+      {children}
+    </SelectedTaskContext.Provider>
+  );
+};
+
+export const useSelectedTask = () => {
+  const context = useContext(SelectedTaskContext);
+  if (!context) {
+    throw new Error("useSelectedTask must be used within a SelectedTaskProvider");
+  }
+  return context;
+};
