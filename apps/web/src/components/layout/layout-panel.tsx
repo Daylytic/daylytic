5f0b7f56833a3e96631ba16f4003f20360483296
iDarkQ <matikuki3@gmail.com>
2025-03-14 16:19:05 +0000

    refactor(layout-panel.tsx): use newly created providers

--- apps/web/src/components/layout/layout-panel.tsx
+++ apps/web/src/components/layout/layout-panel.tsx
@@ -6,19 +6,31 @@ import { TagProvider } from "providers/tag";
 import { Outlet } from "react-router";
 import { Menu } from "components/panel/menu";
 import { GoalProvider } from "providers/goal";
+import { PanelFetcherProvider } from "providers/panel-fetcher";
+import { ProjectProvider } from "providers/project";
+import { TaskProvider } from "providers/task";
+import { SelectedTaskProvider } from "providers/selected-task";
 
 export const LayoutPanel = () => {
   const [cookies] = useCookies(["token"]);
   return (
-    <GoalProvider token={cookies.token}>
-      <DailyTasksProvider token={cookies.token}>
-        <TagProvider token={cookies.token}>
-          <AntLayout id={styles.panel}>
-            <Menu />
-            <Outlet />
-          </AntLayout>
-        </TagProvider>
-      </DailyTasksProvider>
-    </GoalProvider>
+    <PanelFetcherProvider token={cookies.token}>
+      <GoalProvider token={cookies.token}>
+        <ProjectProvider>
+          <TaskProvider>
+            <SelectedTaskProvider>
+              <DailyTasksProvider token={cookies.token}>
+                <TagProvider token={cookies.token}>
+                  <AntLayout id={styles.panel}>
+                    <Menu />
+                    <Outlet />
+                  </AntLayout>
+                </TagProvider>
+              </DailyTasksProvider>
+            </SelectedTaskProvider>
+          </TaskProvider>
+        </ProjectProvider>
+      </GoalProvider>
+    </PanelFetcherProvider>
   );
 };
