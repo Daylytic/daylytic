ebd7de8f53825ebe282a62e93e0e978f7796998c
iDarkQ <matikuki3@gmail.com>
2025-03-14 16:17:40 +0000

    refactor(drop-indicator-line.tsx): use paddingXS to display gap

--- apps/web/src/components/drop-indicator/drop-indicator-line.tsx
+++ apps/web/src/components/drop-indicator/drop-indicator-line.tsx
@@ -3,8 +3,11 @@ import type { CSSProperties } from 'react';
 import styles from './drop-indicator.module.css';
 
 import type { Edge } from '@atlaskit/pragmatic-drag-and-drop-hitbox/types';
+import { theme } from 'antd';
 
 type Orientation = 'horizontal' | 'vertical';
+
+const { useToken } = theme;
 
 const edgeToOrientationMap: Record<Edge, Orientation> = {
   top: 'horizontal',
@@ -26,13 +29,14 @@ export function Line({
   strokeColor?: string;
   strokeWidth?: string;
 }) {
+  const { token } = useToken();
   const orientation = edgeToOrientationMap[edge];
-  const className = `${styles.root} ${styles[orientation]} ${styles[edge]}`;
+  const className = `${styles.root} ${styles[orientation]} ${styles[edge]} .css-var-r1 `;
 
   const style: CSSProperties = {
     '--stroke-color': strokeColor,
     '--stroke-width': strokeWidth,
-    '--main-axis-offset': `calc(-0.5 * (${gap} + var(--stroke-width)))`,
+    '--main-axis-offset': `calc(-0.5 * (${gap ?? token.paddingXS.toString() + "px"} + var(--stroke-width)))`,
     '--line-main-axis-start': indent,
   } as CSSProperties;
 
