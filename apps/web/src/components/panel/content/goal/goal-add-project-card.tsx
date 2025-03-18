f586fb50b3090be5c583c8ae745663c1951ee69a
iDarkQ <matikuki3@gmail.com>
2025-03-18 17:26:39 +0000

    refactor(goal-add-project-card.tsx): fix button not coming to default state after confirming input

--- apps/web/src/components/panel/content/goal/goal-add-project-card.tsx
+++ apps/web/src/components/panel/content/goal/goal-add-project-card.tsx
@@ -4,18 +4,20 @@ import clsx from "clsx";
 import { useState } from "react";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import { useGoal } from "providers/goal";
+import { useProject } from "providers/project";
 
 export const GoalAddProjectCard = () => {
   const [showInput, setShowInput] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [projectName, setProjectName] = useState<string>("");
-  const { getSelectedGoal, createProject } = useGoal();
+  const { getSelectedGoal } = useGoal();
+  const { createProject } = useProject();
 
   const handleHideInput = async () => {
     if (projectName === "") {
       setShowInput(false);
     }
-  }
+  };
 
   const handleInputChange = async (e) => {
     setProjectName(e.target.value);
@@ -24,14 +26,18 @@ export const GoalAddProjectCard = () => {
   const handleCreateProject = async () => {
     setLoading(true);
     await createProject(getSelectedGoal()!.id, projectName);
+    setLoading(false);
+    setProjectName("");
   };
 
   return (
     <>
       {showInput ? (
         <Card
-          className={styles.card}
-          actions={[loading ? <Spin size="small" /> : <PlusCircleOutlined onClick={handleCreateProject} />]}
+          className={styles["project-card-creator"]}
+          actions={[
+            loading ? <Spin size="small" /> : <PlusCircleOutlined onClick={handleCreateProject} />,
+          ]}
         >
           <Input.TextArea
             size="small"
@@ -55,7 +61,7 @@ export const GoalAddProjectCard = () => {
           color="default"
           variant="filled"
           size="large"
-          className={clsx(styles["create-project-button"])}
+          className={styles["project-card-button-creator"]}
         >
           Add A New Project
         </Button>
