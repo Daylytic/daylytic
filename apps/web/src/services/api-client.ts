c610d6d8f2997b591a93568890d81b0a36eade5f
iDarkQ <matikuki3@gmail.com>
2025-03-24 21:22:32 +0000

    feat(api-client.tsx): add error handling within api client

--- apps/web/src/services/api-client.ts
+++
@@ -1,9 +0,0 @@
-import createClient from "openapi-fetch";
-import type { paths } from "lib/api/schema.ts";
-
-export const client = createClient<paths>({
-  baseUrl: "http://localhost:8084/",
-  headers: {
-    Accept: "application/json",
-  },
-});
