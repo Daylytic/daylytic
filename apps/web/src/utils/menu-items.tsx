b75fb4dda22a9282a6ccd677fa6476abc54a7c62
iDarkQ <matikuki3@gmail.com>
2025-04-17 22:04:58 +0100

    feat(web): make sure in menu, timelytic will display how long left in the timer

--- apps/web/src/utils/menu-items.tsx
+++ apps/web/src/utils/menu-items.tsx
@@ -7,8 +7,14 @@ import {
   CheckOutlined,
 } from "@ant-design/icons";
 import { Badge, Tooltip } from "antd";
+import Countdown from "antd/es/statistic/Countdown";
+import dayjs from "dayjs";
+import { useClock } from "~/components/panel/content/timelytic";
 import { GeneralItem } from "~/components/panel/menu/general";
+import { clockFormat } from "~/utils/date";
 import { Routes } from "~/utils/routes";
+import { styles } from "~/components/panel/menu/general";
+import clsx from "clsx";
 
 export const getBadgeColor = (tasksRemaining, currentHour) => {
   if (!tasksRemaining) return "var(--ant-green)";
@@ -25,44 +31,66 @@ const getTooltipText = (tasksRemaining, currentHour) => {
   return "You have very little time. Try to find time to finish your routine.";
 };
 
-export const getGeneralMenuItems = (tasksRemaining, currentHour) => [
-  {
-    key: Routes.PANEL_DASHBOARD,
-    label: <GeneralItem text="Dashboard" />,
-    icon: <ScheduleOutlined />,
-  },
-  {
-    key: Routes.PANEL_ROUTINE,
-    label: (
-      <GeneralItem
-        text="Routine"
-        badge={
-          <Badge
-            count={
-              <Tooltip title={getTooltipText(tasksRemaining, currentHour)}>
-                {tasksRemaining ? (
-                  <ClockCircleOutlined
-                    style={{ color: getBadgeColor(tasksRemaining, currentHour) }}
-                  />
-                ) : (
-                  <CheckOutlined style={{ color: getBadgeColor(tasksRemaining, currentHour) }} />
-                )}
-              </Tooltip>
-            }
-          />
-        }
-      />
-    ),
-    icon: <UnorderedListOutlined />,
-  },
-  {
-    key: Routes.PANEL_CALENDAR,
-    label: <GeneralItem text="Calendar" />,
-    icon: <CalendarOutlined />,
-  },
-  {
-    key: Routes.PANEL_TIMELYTIC,
-    label: <GeneralItem text="Timelytic" />,
-    icon: <AimOutlined />,
-  },
-];
+export const getGeneralMenuItems = (tasksRemaining, currentHour) => {
+  const { percent, deadline, isRunning, remainingTime } = useClock(false);
+
+  return [
+    {
+      key: Routes.PANEL_DASHBOARD,
+      label: <GeneralItem text="Dashboard" />,
+      icon: <ScheduleOutlined />,
+    },
+    {
+      key: Routes.PANEL_ROUTINE,
+      label: (
+        <GeneralItem
+          text="Routine"
+          badge={
+            <Badge
+              count={
+                <Tooltip title={getTooltipText(tasksRemaining, currentHour)}>
+                  {tasksRemaining ? (
+                    <ClockCircleOutlined
+                      style={{ color: getBadgeColor(tasksRemaining, currentHour) }}
+                    />
+                  ) : (
+                    <CheckOutlined style={{ color: getBadgeColor(tasksRemaining, currentHour) }} />
+                  )}
+                </Tooltip>
+              }
+            />
+          }
+        />
+      ),
+      icon: <UnorderedListOutlined />,
+    },
+    {
+      key: Routes.PANEL_CALENDAR,
+      label: <GeneralItem text="Calendar" />,
+      icon: <CalendarOutlined />,
+    },
+    {
+      key: Routes.PANEL_TIMELYTIC,
+      label: (
+        <GeneralItem
+          text="Timelytic"
+          badge={
+            deadline &&
+            (isRunning && percent != 100 ? (
+              <Countdown
+                className={clsx("ant-statistic-content-value", styles.clock)}
+                value={deadline.valueOf()}
+                format={clockFormat}
+              />
+            ) : (
+              <span className="ant-statistic-content-value">
+                {dayjs(Math.max(remainingTime, 0)).format(clockFormat)}
+              </span>
+            ))
+          }
+        />
+      ),
+      icon: <AimOutlined />,
+    },
+  ];
+};
