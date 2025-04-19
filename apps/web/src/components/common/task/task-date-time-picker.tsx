992305acd97d39d8cef199caf9b60fb050451ae1
iDarkQ <matikuki3@gmail.com>
2025-04-19 11:51:26 +0100

    refactor(task-date-time-picker.tsx): remove reset button

--- apps/web/src/components/common/task/task-date-time-picker.tsx
+++ apps/web/src/components/common/task/task-date-time-picker.tsx
@@ -69,11 +69,6 @@ export const TaskDateTimePicker: React.FC<TaskDateTimePickerProps> = ({
             }
           }}
         />
-        {val && (
-          <Button size="small" onClick={handleReset}>
-            Reset
-          </Button>
-        )}
       </Flex>
     );
   }
